727884233faab5887e8506963d69a67587ff28b2
OlegBarabash <olegbarabash7@gmail.com>
2023-09-09 16:26:48 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,40 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 
-//DO HOOKS
-
-export class App extends Component {
-  state = {
+export const App = () => {
+  const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
+    const { good, neutral, bad } = feedback;
     return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    return Math.round((100 / this.countTotalFeedback()) * good);
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((100 / countTotalFeedback()) * feedback.good);
   };
 
-  onClick = e => {
+  const onClick = e => {
     const { name } = e.target;
-    this.setState(prevState => ({ [name]: prevState[name] + 1 }));
+    setFeedback(prevState => ({ ...prevState, [name]: prevState[name] + 1 }));
   };
 
-  render() {
-    return (
-      <Section title={'Please leave feedback'}>
-        <FeedbackOptions
-          options={Object.keys(this.state)}
-          onLeaveFeedback={this.onClick}
-        />
-        <Statistics
-          data={this.state}
-          positivePercentage={this.countPositiveFeedbackPercentage()}
-          total={this.countTotalFeedback()}
-        />
-      </Section>
-    );
-  }
-}
+  return (
+    <Section title={'Please leave feedback'}>
+      <FeedbackOptions
+        options={Object.keys(feedback)}
+        onLeaveFeedback={onClick}
+      />
+      <Statistics
+        data={feedback}
+        positivePercentage={countPositiveFeedbackPercentage()}
+        total={countTotalFeedback()}
+      />
+    </Section>
+  );
+};
